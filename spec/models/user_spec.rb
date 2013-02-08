61d8df749110d4d55c8a98368630d11b81958f80
samuelrails <samuels410@gmail.com>
2013-02-08 10:16:52 +0530

    sign in cmpleted

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 
 	
@@ -39,6 +40,7 @@ describe User do
 		before { @user.name = "a" * 51 }
 		it { should_not be_valid }
 	end
+	
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
@@ -48,6 +50,7 @@ describe User do
 			end
 		end
 	end
+	
 	describe "when email format is valid" do
 		it "should be valid" do
 			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
@@ -57,6 +60,7 @@ describe User do
 			end
 		end
 	end
+	
 	describe "when email address is already taken" do
 		before do
 			user_with_same_email = @user.dup
@@ -70,32 +74,42 @@ describe User do
 		before { @user.password = @user.password_confirmation = " " }
 		it { should_not be_valid }
 	end
+	
 	describe "when password doesn't match confirmation" do
 		before { @user.password_confirmation = "mismatch" }
 		it { should_not be_valid }
 	end
+	
 	describe "when password confirmation is nil" do
 		before { @user.password_confirmation = nil }
 		it { should_not be_valid }
 	end
+	
 	describe "return value of authenticate method" do
 		before { @user.save }
 		let(:found_user) { User.find_by_email(@user.email) }
 		describe "with valid password" do
 			it { should == found_user.authenticate(@user.password) }
 		end
+		
 		describe "with invalid password" do
 			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 			it { should_not == user_for_invalid_password }
 			specify { user_for_invalid_password.should be_false }
 		end
 	end
+	
 	describe "email address with mixed case" do
 		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
-	it "should be saved as all lower-case" do
-		@user.email = mixed_case_email
-		@user.save
-		@user.reload.email.should == mixed_case_email.downcase
-end
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
 end
 end
